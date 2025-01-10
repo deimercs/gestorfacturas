0dc1559af63ceaaf4b84d957ff85e4477b65165b
Raven <deimercs@gmail.com>
2025-01-10 18:43:41 -0500

    modificando archivos de docker para que apunte  las variables de entorno correctas

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,13 @@ module.exports = defineConfig({
   configureWebpack: {
     resolve: {
       fallback: {
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        util: require.resolve('util/'),
-        url: require.resolve('url/'),
-        stream: require.resolve('stream-browserify'),
-        zlib: require.resolve('browserify-zlib')
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "util": require.resolve("util/"),
+        "zlib": require.resolve("browserify-zlib"),
+        "stream": require.resolve("stream-browserify"),
+        "url": require.resolve("url/"),
+        "buffer": require.resolve("buffer/")
       }
     },
     plugins: [
