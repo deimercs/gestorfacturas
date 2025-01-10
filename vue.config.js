61bd6cdedf1d65671422b1a036af68eaa23c4972
Raven <deimercs@gmail.com>
2025-01-10 15:34:05 -0500

    se modifica archivo vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      allowedHosts: ['.onrender.com'], // ¡Esta es la línea clave!
+      // O, si prefieres una opción menos segura (solo para pruebas en Render, NO para producción real):
+      // allowedHosts: 'all',
+    },
+  },
+});
